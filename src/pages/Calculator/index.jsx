8d6ce8f55c60c3ee6c253e38fb22406a8503bd90
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-03-27 22:10:34 +0200

    Calculator Styling

--- src/pages/Calculator/index.jsx
+++ src/pages/Calculator/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import './style.css'
 
-
 const Calculator = () => {
     const [displayValue, setDisplayValue] = useState('0');
 
@@ -14,36 +13,45 @@ const Calculator = () => {
     }
 
     const calculate = () => {
-        const result = eval(displayValue);
-        setDisplayValue(result);
+        try {
+            const result = eval(displayValue);
+            setDisplayValue(result);
+        } catch (error) {
+            setDisplayValue('Error');
+        }
     }
 
     return (
-        <div className='calculator'>
-            <input type='text' value={displayValue} readOnly />
-            <div className='buttons'>
-                <button onClick={() => handleInput('1')}>1</button>
-                <button onClick={() => handleInput('2')}>2</button>
-                <button onClick={() => handleInput('3')}>3</button>
-                <button onClick={() => handleInput('4')}>4</button>
-                <button onClick={() => handleInput('5')}>5</button>
-                <button onClick={() => handleInput('6')}>6</button>
-                <button onClick={() => handleInput('7')}>7</button>
-                <button onClick={() => handleInput('8')}>8</button>
-                <button onClick={() => handleInput('9')}>9</button>
-                <button onClick={() => handleInput('0')}>0</button>
-            </div>
-            <div className='operator'>
-                <button onClick={() => handleInput('+')}>+</button>
-                <button onClick={() => handleInput('-')}>-</button>
-                <button onClick={() => handleInput('*')}>x</button>
-                <button onClick={() => handleInput('/')}>/</button>
-            </div>
-            <div className='equal'>
-                <button onClick={calculate}>=</button>
-            </div>
-            <div className='clear'>
-                <button onClick={() => setDisplayValue('0')}>C</button>
+        <div className='main-app'>
+            <h1 className='header'>Calculator App</h1>
+            <div className='calculator'>
+                <input type='text' value={displayValue} readOnly />
+                <div className='buttons'>
+                    <button onClick={() => handleInput('7')}>7</button>
+                    <button onClick={() => handleInput('8')}>8</button>
+                    <button onClick={() => handleInput('9')}>9</button>
+                    <button onClick={() => handleInput('/')}>/</button>
+                    <button onClick={() => handleInput('4')}>4</button>
+                    <button onClick={() => handleInput('5')}>5</button>
+                    <button onClick={() => handleInput('6')}>6</button>
+                    <button onClick={() => handleInput('*')}>x</button>
+                    <button onClick={() => handleInput('1')}>1</button>
+                    <button onClick={() => handleInput('2')}>2</button>
+                    <button onClick={() => handleInput('3')}>3</button>
+                    <button onClick={() => handleInput('-')}>-</button>
+                    <button onClick={() => handleInput('.')}>.</button>
+                    <button onClick={() => handleInput('0')}>0</button>
+                    <div className='clear'>
+                        <button onClick={() => setDisplayValue('0')}>C</button>
+                    </div>
+                    <button onClick={() => handleInput('+')}>+</button>
+                </div>
+                <div className='operator'>
+                </div>
+                <div className='equal'>
+                    <button onClick={calculate}>=</button>
+                </div>
+
             </div>
         </div>
     )
