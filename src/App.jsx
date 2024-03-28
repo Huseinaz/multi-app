1ad76d9a3d387a2834ec5b84f461734d3429184e
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-03-28 23:12:07 +0200

    Sticky Notes init

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Calculator from "./pages/Calculator";
+import StickyNotes from "./pages/StickyNotes";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/weather" element={<FirstApp />} />
           <Route path="/calculator" element={<Calculator />} />
+          <Route path="/stickynotes" element={<StickyNotes />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/StickyNotes/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+const StickyNotes = () => {
+    return (
+        <div className='container'>
+            <h1>Sticky Notes</h1>
+        </div>
+    )
+}
+
+export default StickyNotes
